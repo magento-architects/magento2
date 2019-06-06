60820bc7aad4bb7955e828901e0089e7ec3464f6
Igor Melnykov <melnykov@adobe.com>
2019-06-05 20:25:34 -0500

    Workarounds for distributed deployment

--- lib/internal/Magento/Framework/Module/Dir/Reader.php
+++ lib/internal/Magento/Framework/Module/Dir/Reader.php
@@ -155,7 +155,7 @@ class Reader
             foreach ($recursiveIterator as $actionFile) {
                 $actionName = str_replace('/', '\\', str_replace($actionDir, '', $actionFile->getPathname()));
                 $action = $namespace . "\\" . Dir::MODULE_CONTROLLER_DIR . substr($actionName, 0, -4);
-                $actions[strtolower(str_replace('AdminUi', '', $action))] = str_replace('AdminUi', '', $action);
+                $actions[strtolower(preg_replace("/([^\\\\]){1}(Ui|AdminUi)/", "$1", $action))] = preg_replace("/([^\\\\]){1}(Ui|AdminUi)/", "$1", $action);
             }
         }
         return $actions;
